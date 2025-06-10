d16e31e90e8a03043e9ddcfbf5519c607a64a2c2
Munisa <munisa@Munisas-MacBook-Pro.local>
2025-06-10 14:45:35 +0500

    done

--- src/components/Page4/index.jsx
+++ src/components/Page4/index.jsx
@@ -11,9 +11,9 @@ export default function Page4() {
     <div className="page4">
         <h3>Menu</h3>
         <h1>Food Full of treaty Love</h1>
-        <p>There are many things are needed to start the Fast Food Business. You need not only Just Food Stalls with Persons but also specialized equipment, Skills to manage Customers, </p>
-        <div className="cards">
-            <div className="card">
+        <p className="description4">There are many things are needed to start the Fast Food Business. You need not only Just Food Stalls with Persons but also specialized equipment, Skills to manage Customers, </p>
+        <div className="cards4">
+            <div className="card4">
                 <img src={img6} alt="" />
                 <div className="card-price">
                     <h1>Vegie Muffen</h1>
@@ -23,7 +23,7 @@ export default function Page4() {
                 <h1 className="plus">+</h1>
             </div>
 
-            <div className="card">
+            <div className="card4">
                 <img src={img7} alt="" />
                 <div className="card-price">
                     <h1>Salads</h1>
@@ -33,7 +33,7 @@ export default function Page4() {
                 <h1 className="plus">+</h1>
             </div>
 
-            <div className="card">
+            <div className="card4">
                 <img src={img8} alt="" />
                 <div className="card-price">
                     <h1>Burger</h1>
@@ -43,7 +43,7 @@ export default function Page4() {
                 <h1 className="plus">+</h1>
             </div>
 
-            <div className="card">
+            <div className="card4">
                 <img src={img9} alt="" />
                 <div className="card-price">
                     <h1>Delmonico Steak dish</h1>
@@ -53,7 +53,7 @@ export default function Page4() {
                 <h1 className="plus">+</h1>
             </div>
 
-            <div className="card">
+            <div className="card4">
                 <img src={img10} alt="" />
                 <div className="card-price">
                     <h1>Egg Masala</h1>
@@ -63,7 +63,7 @@ export default function Page4() {
                 <h1 className="plus">+</h1>
             </div>
 
-            <div className="card">
+            <div className="card4">
                 <img src={img11} alt="" />
                 <div className="card-price">
                     <h1>Peach Melba dish</h1>
